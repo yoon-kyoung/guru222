99615e71acbf7d678dd5613c9505c1b516e819ff
HaNeul0219 <haley0219@naver.com>
2023-08-01 02:49:42 +0900

    search update

---
+++ app/src/main/java/com/example/myapplication/SearchingActivity111.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchingActivity111: AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.searching111)
+    }
+}
