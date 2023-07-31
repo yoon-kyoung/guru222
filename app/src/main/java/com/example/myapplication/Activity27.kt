4781e06c83022d7d00b699f3038e08a5f4f514b8
HaNeul0219 <haley0219@naver.com>
2023-08-01 01:51:07 +0900

    memory update

---
+++ app/src/main/java/com/example/myapplication/Activity27.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Activity27 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search2)
+
+    }
+}
